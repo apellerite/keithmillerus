0b93e52a0f108f14c8433972be2771970bfe136c
keith miller <keith989sports@hotmail.com>
2014-08-17 17:59:56 -0400

    templates added

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Site Down For Maintenance'
+    #return 'Site Down For Maintenance'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
