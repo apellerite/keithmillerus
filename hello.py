b332e4754e8c934d28422b2ab4187e1aedfbed12
keith miller <keith989sports@hotmail.com>
2014-08-17 18:09:22 -0400

    bootstrap added

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1> Hello, %s</h1>' % name
+    return render_template('user.html')
 
 if __name__=='__main__':
     app.run()
